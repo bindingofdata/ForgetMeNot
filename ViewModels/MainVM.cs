365a30f33909dcd85c1544fb2f56a14bc130b172
Hunter Curren <hcurren82@gmail.com>
2023-12-05 14:13:18 -0800

    Image button and gesture recognizers.

--- ViewModels/MainVM.cs
+++ ViewModels/MainVM.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ForgetMeNot.ViewModels
 {
@@ -12,6 +13,23 @@ namespace ForgetMeNot.ViewModels
     public partial class MainVM
     {
         [ObservableProperty]
+        private bool imageIsVisible = true;
+
+        [ObservableProperty]
 		private string _fullName;
+        [ObservableProperty]
+        private string _selectedImage = "ministry_of_information.jpg";
+
+        [RelayCommand]
+        private void ToggleImageVisibility()
+        {
+            ImageIsVisible = !ImageIsVisible;
+        }
+
+        [RelayCommand]
+        private void ImageTapped()
+        {
+            ImageIsVisible = !ImageIsVisible;
+        }
     }
 }
